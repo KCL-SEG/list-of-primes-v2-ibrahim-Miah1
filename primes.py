5c798970c493214417f621ddfe0ded395a617436
Ibrahim Miah <k21004632@kcl.ac.uk>
2023-12-06 18:48:51 +0000

    task completed

--- primes.py
+++ primes.py
@@ -1,6 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
 def primes(number_of_primes):
+    
+    if number_of_primes <= 0:
+        raise ValueError("The number must be greater than 0")
+    
+    
     list = []
+    current_number = 2
+
+    while len(list) < number_of_primes:
+        if is_prime(current_number):
+            list.append(current_number)
+        current_number += 1
+
     return list
